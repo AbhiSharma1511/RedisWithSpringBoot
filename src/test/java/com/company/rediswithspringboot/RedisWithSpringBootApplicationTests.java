6fd886a3acfa45b7ff4ec218611bb8435e8eacea
Abhinav Sharma <abhisharmaa9105@gmail.com>
2026-08-30 20:33:53 +0530

    project setup

---
+++ src/test/java/com/company/rediswithspringboot/RedisWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.rediswithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
